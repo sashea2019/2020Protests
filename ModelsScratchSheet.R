e736906594e3a2d24ce0daf9ccb9200ad10192d6
Arica <77238032+arica-schuett@users.noreply.github.com>
2025-03-19 18:20:43 -0400

    Add files via upload
    Code that updates the primary analysis data and early-stage models of exploratory models

--- ModelsScratchSheet.R
+++ ModelsScratchSheet.R
@@ -1,36 +1,42 @@
-ProtestByCity <- read.csv("/Users/aricaschuett/Documents/protest/ProtestByCity01-01-2017-12-31-20.csv")
+ProtestByCity <- read.csv("/Users/aricaschuett/Documents/protest/ProtestByCity3-19.csv", row.names = NULL)
 
 
-ProtestLimited <- lm(ProtestCount ~ population , data = ProtestByCity)
-nobs(ProtestLimited) #1045obs
+ProtestLimited <- lm(PostGFProtestCount ~ population , data = ProtestByCity)
+nobs(ProtestLimited) #2874
 summary(ProtestLimited)
 
-ProtestKitchenSink <- lm(ProtestCount ~ population + BlackPop + BlackPov + CollegeEdTotal + VictimsCountPreGF, data = ProtestByCity)
-nobs(ProtestKitchenSink) #712 obs
+ProtestKitchenSink <- lm(PostGFProtestCount ~ population + BlackPop + BlackPov + CollegeEdTotal + VictimsPreGF2020, data = ProtestByCity)
+nobs(ProtestKitchenSink) #221obs
 used_data <- model.frame(ProtestKitchenSink)
-dropped_obs <- anti_join(ProtestByCity, used_data, by = c("ProtestCount","VictimsCountPreGF"))
+dropped_obs <- anti_join(ProtestByCity, used_data, by = c("PostGFProtestCount","VictimsPreGF2020"))
 summary(ProtestKitchenSink)
 
-ProtestKitchenSink <- lm(ProtestCount ~ population + BlackPop + BlackPov + CollegeEdTotal + VictimsCountPreGF, data = ProtestByCity)
+ProtestKitchenSink <- lm(PostGFProtestCount ~ population + BlackPop + BlackPov + CollegeEdTotal + VictimsPreGF2020, data = ProtestByCity)
 summary(ProtestKitchenSink)
 
-ProtestKitchenSinkPct <- lm(ProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + VictimsCountPreGF, data = ProtestByCity)
+ProtestKitchenSinkPct <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + VictimsPreGF2020, data = ProtestByCity)
 summary(ProtestKitchenSinkPct)
 
-ProtestKitchenSinkPct <- lm(ProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + VictimsCountPreGFBlack, data = ProtestByCity)
+ProtestKitchenSinkPct <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + BlackVictimCountPreGF, data = ProtestByCity)
 summary(ProtestKitchenSinkPct)
 
-ProtestByCity$PoliceDeathsRate <- ProtestByCity$VictimsCountPreGF/ProtestByCity$population
-ProtestByCity$BlackVictimsRate <- ProtestByCity$VictimsCountPreGFBlack/ProtestByCity$population
+ProtestKitchenSinkPct <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + BlackVictimCountPreGF, data = ProtestByCity)
+summary(ProtestKitchenSinkPct)
 
-ProtestKitchenSinkPctDeathRate <- lm(ProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + PoliceDeathsRate, data = ProtestByCity)
+ProtestKitchenSinkPct <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + AntiTrumpProtestPreGFCount + BlackVictimCountPreGF, data = ProtestByCity)
+summary(ProtestKitchenSinkPct)
+
+ProtestByCity$PoliceDeathsRate <- ProtestByCity$VictimCountPreGF/ProtestByCity$population
+ProtestByCity$BlackVictimsRate <- ProtestByCity$VictimCountPreGF/ProtestByCity$population
+
+ProtestKitchenSinkPctDeathRate <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + AntiTrumpProtestPreGFCount+ PoliceDeathsRate, data = ProtestByCity)
 summary(ProtestKitchenSinkPctDeathRate)
 
-ProtestKitchenSinkPctDeathRateBlk <- lm(ProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + BlackVictimsRate, data = ProtestByCity)
+ProtestKitchenSinkPctDeathRateBlk <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + BlackVictimsRate, data = ProtestByCity)
 summary(ProtestKitchenSinkPctDeathRateBlk)
 
-ProtestByCity$VictimRaceRatio <- ProtestByCity$VictimsCountPreGFBlack/ ProtestByCity$VictimsCountPreGF
-ProtestKitchenSinkPctDeathRaceRatio <- lm(ProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + VictimRaceRatio, data = ProtestByCity)
+ProtestByCity$VictimRaceRatio <- ProtestByCity$BlackVictimCountPreGF/ ProtestByCity$VictimsCountPreGF
+ProtestKitchenSinkPctDeathRaceRatio <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + VictimRaceRatio, data = ProtestByCity)
 summary(ProtestKitchenSinkPctDeathRaceRatio)
 
 
@@ -53,16 +59,18 @@ summary(ProtestKitchenSinkPct)
 ProtestKitchenSinkPct <- lm(PostGFProtest ~ population + BlackPopPct + BlackPovRate + EduRate + VictimsCountPreGFBlack, data = ProtestByCity)
 summary(ProtestKitchenSinkPct)
 
-ProtestByCity$PoliceDeathsRate <- ProtestByCity$VictimsCountPreGF/ProtestByCity$population
-ProtestByCity$BlackVictimsRate <- ProtestByCity$VictimsCountPreGFBlack/ProtestByCity$population
 
-ProtestKitchenSinkPctDeathRate <- lm(PostGFProtest ~ population + BlackPopPct + BlackPovRate + EduRate + PoliceDeathsRate, data = ProtestByCity)
+ProtestByCity$PoliceDeathsRate <- ProtestByCity$VictimCountPreGF/ProtestByCity$population
+ProtestByCity$BlackVictimsRate <- ProtestByCity$BlackVictimCountPreGF/ProtestByCity$population
+
+ProtestKitchenSinkPctDeathRate <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + PoliceDeathsRate, data = ProtestByCity)
 summary(ProtestKitchenSinkPctDeathRate)
 
-ProtestKitchenSinkPctDeathRateBlk <- lm(PostGFProtest ~ population + BlackPopPct + BlackPovRate + EduRate + BlackVictimsRate, data = ProtestByCity)
+ProtestKitchenSinkPctDeathRateBlk <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + BlackVictimsRate, data = ProtestByCity)
 summary(ProtestKitchenSinkPctDeathRateBlk)
 
-ProtestByCity$VictimRaceRatio <- ProtestByCity$VictimsCountPreGFBlack/ ProtestByCity$VictimsCountPreGF
-ProtestKitchenSinkPctDeathRaceRatio <- lm(PostGFProtest ~ population + BlackPopPct + BlackPovRate + EduRate + VictimRaceRatio, data = ProtestByCity)
+ProtestByCity$VictimRaceRatio <- ProtestByCity$BlackVictimCountPreGF/ ProtestByCity$VictimCountPreGF
+
+ProtestKitchenSinkPctDeathRaceRatio <- lm(PostGFProtestCount ~ population + BlackPopPct + BlackPovRate + EduRate + VictimRaceRatio, data = ProtestByCity)
 summary(ProtestKitchenSinkPctDeathRaceRatio)
 
